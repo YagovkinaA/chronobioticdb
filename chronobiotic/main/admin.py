eb839bc75beb0ae29ead47c0784d617769cf8b05
SoalRin <wakeupstardust@gmail.com>
2024-11-27 08:59:18 +0300

    changes db

--- chronobiotic/main/admin.py
+++ chronobiotic/main/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import  Chronobiotic, Clinicaltrials, Bioclass, Mechanism,Synonyms
+from .models import  Chronobiotic, Targets, Bioclass, Mechanism,Synonyms
 
 @admin.register(Chronobiotic)
 class Chronobioadm(admin.ModelAdmin):
     prepopulated_fields = {'linkname':('gname',)}
 #admin.site.register(Chronobiotic)
-admin.site.register(Clinicaltrials)
+# admin.site.register(Clinicaltrials)
 admin.site.register(Bioclass)
 admin.site.register(Mechanism)
 admin.site.register(Synonyms)
+admin.site.register(Targets)
--- chronobiotic/main/models.py
+++ chronobiotic/main/models.py
@@ -17,6 +17,20 @@ class Synonyms(models.Model):
         String for representing the Model object (in Admin site etc.)
         """
         return self.synonymsmname
+class Targets(models.Model):
+    """
+    Model representing a chem class (e.g. chronobiotics, radioprotectors).
+    """
+    targetsname = models.CharField(max_length=128, help_text="Enter a target(e.g. relax, energy, tea , coffe)")
+    targeturl = models.URLField(max_length=200, help_text="Enter a link about this target")
+    class Meta:
+        db_table: str='target'
+
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return self.targetsmname
 class Mechanism(models.Model):
     """
     Model representing a chem class (e.g. chronobiotics, radioprotectors).
@@ -43,19 +57,20 @@ class Bioclass(models.Model):
         String for representing the Model object (in Admin site etc.)
         """
         return self.nameclass
-class Clinicaltrials(models.Model):
-    """
-    Model representing a chem trials (e.g. humans,mouse,flyers).
-    """
-    trialsname = models.URLField(max_length=200, help_text="Enter a link about trials")
-    class Meta:
-        db_table: str='trials'
 
-    def __str__(self):
-        """
-        String for representing the Model object (in Admin site etc.)
-        """
-        return self.trialsname
+# class Clinicaltrials(models.Model):
+#     """
+#     Model representing a chem trials (e.g. humans,mouse,flyers).
+#     """
+#     trialsname = models.URLField(max_length=200, help_text="Enter a link about trials")
+#     class Meta:
+#         db_table: str='trials'
+#
+#     def __str__(self):
+#         """
+#         String for representing the Model object (in Admin site etc.)
+#         """
+#         return self.trialsname
 class Chronobiotic(models.Model):
 
     gname = models.CharField(max_length=128, unique=True)
@@ -67,24 +82,26 @@ class Chronobiotic(models.Model):
     description = models.TextField(max_length=1000, help_text="Enter a  description of the biotic",blank=True)
     fdastatus = models.CharField(max_length=64,null=True,blank=True)
     article= models.URLField(max_length = 200,null=True,blank=True)
+    linkslists=models.URLField(max_length=200,null=True,blank=True)
     pubchem = models.URLField(max_length=200,null=True,blank=True)
     chemspider = models.URLField(max_length=200, null=True,blank=True)
     drugbank = models.URLField(max_length=200, null=True,blank=True)
     chebi = models.URLField(max_length=200, null=True,blank=True)
-    chembil = models.URLField(max_length=200, null=True,blank=True)
+    # chembil = models.URLField(max_length=200, null=True,blank=True)
     uniprot = models.URLField(max_length=200, null=True,blank=True)
-    engage = models.URLField(max_length=200, null=True,blank=True)
+    # engage = models.URLField(max_length=200, null=True,blank=True)
     kegg = models.URLField(max_length=200, null=True,blank=True)
-    msds = models.URLField(max_length=200, null=True,blank=True)
-    sider = models.URLField(max_length=200, null=True,blank=True)
-    toxnet = models.URLField(max_length=200, null=True,blank=True)
+    # msds = models.URLField(max_length=200, null=True,blank=True)
+    # sider = models.URLField(max_length=200, null=True,blank=True)
+    # toxnet = models.URLField(max_length=200, null=True,blank=True)
     selleckchem = models.URLField(max_length=200, null=True,blank=True)
     # isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     classf = models.ManyToManyField(Bioclass, help_text="Select a class for this molecula")
     mechanisms = models.ManyToManyField(Mechanism, help_text="Select a synonyms of this ")
+    target = models.ManyToManyField(Targets, help_text="Select a targets of this ")
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
-    # Genre class has already been defined so we can specify the object above.
-    clinictrials = models.ManyToManyField(Clinicaltrials, help_text="Select a class for this biotic")
+
+    # clinictrials = models.ManyToManyField(Clinicaltrials, help_text="Select a class for this biotic")
     class Meta:
         db_table: str='chronobiotic'
 
