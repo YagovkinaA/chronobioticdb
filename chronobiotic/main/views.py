06ee364b0a8ccebc46bf15e4092d0d56bbd4f477
SoalRin <wakeupstardust@gmail.com>
2025-03-11 17:40:04 +0300

    справлена проблема с изображением , теперь все должно отображаться правильно

--- chronobiotic/main/views.py
+++ chronobiotic/main/views.py
@@ -1,18 +1,10 @@
-# from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Chronobiotic
-from django.views import generic
-
-# class ChronoListView(generic.ListView):
-#     model = Chronobiotic
-#     context_object_name ='chem_list'
-#     def get_queryset(self):
-#         return Chronobiotic.objects.all()
 
 def index(request):
-    query = request.GET.get('search', '')  # Получаем поисковый запрос из параметра search
+    query = request.GET.get('search', '')  # Получаем поисковый запрос
     if query:
-        # Фильтруем по имени, формуле или статусу
         model = Chronobiotic.objects.filter(
             gname__icontains=query
         ) | Chronobiotic.objects.filter(
@@ -21,27 +13,14 @@ def index(request):
             fdastatus__icontains=query
         )
     else:
-        # Если нет поискового запроса, показываем всё
         model = Chronobiotic.objects.all()
 
-    return render(request, 'main/index.html', {'model': model, 'query': query})
+    # Пагинация
+    paginator = Paginator(model, 25)  # По 25 записей на страницу
+    page_number = request.GET.get('page')  # Получаем текущую страницу из запроса
+    page_obj = paginator.get_page(page_number)  # Получаем объект страницы
+
+    return render(request, 'main/index.html', {'page_obj': page_obj, 'query': query})
+
 def about(request):
-    return render(request,'main/about.html')
-# def chrono(request):
-#     model = Chronobiotic.objects.all()
-#     # context={
-#     #   model:model,
-#     # }
-#     return render(request,'main/index.html',{'model':model})
-# def index(request)->HttpResponse:
-#     return render(request,'main/index.html')
-# def about(request)->HttpResponse:
-#     return render(request,'main/about.html')
-# def index(request):
-
-#     context = {
-#         'title': 'Home - Главная',
-#         'content': "HOME",
-#     }
-
-#     return render(request, 'main/index.html', context)
+    return render(request, 'main/about.html')
